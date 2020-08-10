923559054ea67cfc69b63e979d4117e33b706ca7
Kieran Gill <kieran.gill1997@gmail.com>
2020-08-09 19:33:13 -0700

    add selection indicator

--- src/pages/labeler/_lib/ColorSelector.svelte
+++ src/pages/labeler/_lib/ColorSelector.svelte
@@ -15,11 +15,12 @@
 <div class='row'>
     {#each Object.entries(COLORS) as [id, color]}
         <div 
-            class='item'
+            class='item'            
             on:click={updateTool(id)}
         >
             <div 
                 class='bubble' 
+                class:selected={$tool.brushType === id}
                 style='background: {color}'
             ></div>
 
@@ -37,6 +38,9 @@
     }
 
     .item {
+        --bubbleSize: 32px;
+        --bubbleInnerShadow: inset -3px -3px 0px rgba(0, 0, 0, 0.12);
+
         display: flex;
         flex-direction: column;
         justify-content: center;
@@ -44,8 +48,19 @@
     }
 
     .bubble {
-        --bubbleSize: 44px;
         width: var(--bubbleSize);
         height: var(--bubbleSize);
+        border: none;
+        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25), 
+            var(--bubbleInnerShadow);
+        transition: all 150ms ease-out;
+    }
+
+    .selected {
+        --whiteSelectionBorder: 0px 0px 0px 2px var(--white);
+        
+        border: 3px solid var(--background);
+        box-shadow: var(--bubbleInnerShadow),
+            var(--whiteSelectionBorder);
     }
 </style>
