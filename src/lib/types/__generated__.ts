46d746fded69898ba48717e65bb432cfcecec444
Kieran Gill <kieran.gill1997@gmail.com>
2020-08-10 20:28:02 -0700

    codegen

---
+++ src/lib/types/__generated__.ts
@@ -0,0 +1,41 @@
+/* eslint-disable */
+export type Maybe<T> = T | null;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
+
+export type Query = {
+  __typename?: 'Query';
+  image: Image;
+  unclaimed: Image;
+};
+
+
+export type QueryImageArgs = {
+  id: Scalars['ID'];
+};
+
+export type Image = {
+  __typename?: 'Image';
+  id: Scalars['ID'];
+  status: Status;
+  labeller?: Maybe<Scalars['String']>;
+  reviewedBy?: Maybe<Scalars['String']>;
+  mergedBy?: Maybe<Scalars['String']>;
+  /** Comment / Issue / PR link */
+  github?: Maybe<Scalars['String']>;
+  url: Scalars['String'];
+};
+
+export enum Status {
+  Merged = 'Merged',
+  InProgress = 'InProgress',
+  Unclaimed = 'Unclaimed'
+}
