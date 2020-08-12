1106932c23caeee8230dffdab20cb8b59b32d269
Kieran Gill <kieran.gill1997@gmail.com>
2020-08-12 13:45:31 -0700

    search radius param and better visibility of results

--- src/pages/test.svelte
+++ src/pages/test.svelte
@@ -3,22 +3,17 @@
     import { onMount } from 'svelte'
     import { convertUrlToImageData } from './_test'
 
-    let image: HTMLImageElement 
-    let canvas: HTMLCanvasElement
-    let ctx: CanvasRenderingContext2D    
-
     const WIDTH = 600
     const HEIGHT = 400
 
-    let blur_radius = 1
-    let low_threshold = 20
-    let high_threshold = 50
-
-    $: image?.src 
-        && blur_radius 
-        && low_threshold 
-        && high_threshold 
-        && renderLines() 
+    let image: HTMLImageElement 
+    let canvas: HTMLCanvasElement
+    let ctx: CanvasRenderingContext2D    
+    let m = { x: 300, y: 300 }
+    let blurRadius = 1
+    let lowThreshold = 20
+    let highThreshold = 50
+    let searchSpace = 350
 
     onMount(async () => {
         const im = '0005_836d09212ac1b8fa_2018-06-15--15-57-15_23_345.png'
@@ -26,22 +21,21 @@
         const result = await convertUrlToImageData(url)
         image.src = result
         ctx = canvas.getContext('2d')
-    })
+    })    
 
-    let m = { x: 300, y: 300 };
-
-	function handleMousemove(event) {
-		m.x = event.clientX;
-        m.y = event.clientY;
+	const handleMousemove = ({ clientX, clientY }) => {
+        const { x, y } = canvas.getBoundingClientRect()
+		m.x = clientX - x
+        m.y = clientY - y
         renderLines()
 	}
 
     function renderLines() {
-        // const start = m // path.points[0] x & y
-        const size = 50  // cursor x & y 
+        // The larger the more accurate the line detection but slower
+        const size = 350
         const start = {
-            x: Math.floor(m.x - (size)),
-            y: Math.floor(m.y - (size))
+            x: Math.floor(m.x - (size/2)),
+            y: Math.floor(m.y - (size/2))
         }
         const searchRadius = 20 /* brushSize */ / 2 // get points in this radius
 
@@ -49,12 +43,12 @@
 
         ctx.drawImage(image, 0, 0, WIDTH, HEIGHT)
         const imageData = ctx.getImageData(start.x, start.y, size, size)
-        const r = blur_radius|0;
-        const kernel_size = (r+1) << 1;
+        const r = blurRadius|0;
+        const kernel_size = (r + 1) << 1;
         
         jsfeat.imgproc.grayscale(imageData.data, size, size, img_u8);
         jsfeat.imgproc.gaussian_blur(img_u8, img_u8, kernel_size, 0);
-        jsfeat.imgproc.canny(img_u8, img_u8, low_threshold|0, high_threshold|0);
+        jsfeat.imgproc.canny(img_u8, img_u8, lowThreshold|0, highThreshold|0);
 
         // render result back to canvas
         const data_u32 = new Uint32Array(imageData.data.buffer);
@@ -66,46 +60,50 @@
 
         while(--i >= 0) {
             pix = img_u8.data[i]
-            //              -1 for white
-            const color = alpha | (pix << 16) | (pix << 8) | pix
+            const color = alpha | (pix << 16) | (pix << 8) | pix // -1 for white
             data_u32[i] = color
 
             if (color == -1) {
-                const col = Math.floor(i % size)
-                const row = (i - col) / size
-                points.push({ row, col })
-                console.count('-1')
-                console.log(i, 'row', row, 'col', col)                
+                const y = Math.floor(i % size)
+                const x = (i - y) / size
+                if (withinSearchRadius(x, y)) {   
+                    points.push({ x, y })
+                }
             }
         }
 
-        const debugBlue = alpha | (0xff << 16) | (0x00 << 8) | 0x00
+        const debugRed = alpha | (0x00 << 16) | (0x00 << 8) | 0xff
 
-        points.forEach(({ row, col }) => {
-            data_u32[(row * size) + col] = debugBlue
-            // console.log(data_u32[row*col])
-            // console.count('1')
+        points.forEach(({ x, y }) => {
+            data_u32[(x * size) + y] = debugRed
         })
 
-        console.countReset('-1')
-        console.countReset('1')
+        function withinSearchRadius(x, y) {
+            const absoluteX = start.x + x
+            const absoluteY = start.y + y
 
-        console.log(img_u8.cols, img_u8.rows)
+            return m.x < absoluteX + searchRadius
+                && m.x > absoluteX - searchRadius
+                && m.y < absoluteY + searchRadius
+                && m.y > absoluteY - searchRadius
+        }
 
         ctx.putImageData(imageData, start.x, start.y)
     }
 
 </script>
 
-<input type="range" min=0 max=10 step={0.05} bind:value={blur_radius}>
-<input type="range" min=1 max=100 step={1} bind:value={low_threshold}>
-<input type="range" min=1 max=100 step={1} bind:value={high_threshold}>
+<input type="range" min=0 max=10 step={0.05} bind:value={blurRadius}>
+<input type="range" min=1 max=100 step={1} bind:value={lowThreshold}>
+<input type="range" min=1 max=100 step={1} bind:value={highThreshold}>
+<input type="range" min=1 max=100 step={1} bind:value={searchSpace}>
 
 <br>
 
-blur radius: {blur_radius}
-low: {low_threshold}
-high: {high_threshold}
+blur radius: {blurRadius}
+low: {lowThreshold}
+high: {highThreshold}
+search space: {searchSpace}
 
 <canvas 
     width={WIDTH} height={HEIGHT} bind:this={canvas}
@@ -117,5 +115,9 @@ high: {high_threshold}
     img, canvas {
         width: 600px;
         height: 400px;
+    }
+
+    canvas {
+        cursor: none;
     }
 </style>
