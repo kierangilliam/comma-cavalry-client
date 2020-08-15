abd878a134d19c02eb8940b8aafa841e5522f6ad
Kieran Gill <kieran.gill1997@gmail.com>
2020-08-15 08:58:45 -0700

    add dark gray

--- src/lib/components/theme/ThemeOverrides.svelte
+++ src/lib/components/theme/ThemeOverrides.svelte
@@ -14,6 +14,7 @@
     :global(:root) {
         --black: #181818;
         --lightGray: #D9D7E0;
+        --darkGray: #373737;
 
         /* These colors are defined using js */        
         --white: magenta;
