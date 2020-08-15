7d872f333b0a121873ffc9ab39e6766bedf0b335
Kieran Gill <kieran.gill1997@gmail.com>
2020-08-15 11:22:46 -0700

    fix test

--- cypress/integration/home.spec.ts
+++ cypress/integration/home.spec.ts
@@ -20,14 +20,13 @@ describe('home', () => {
             }
         }))
         cy.reload()
+
         cy.get('p').contains('in progress')
         cy.get('img').should('be.visible')
-        cy.wait(400)
-        // @ts-ignore TODO FIX
+        cy.wait(700)
         cy.get('body').toMatchImageSnapshot()
     })
 
-    // TODO label new image then navigate back, should show new in progress
     it('navigates to labeler', () => {
         cy.get('button').click()
         cy.location('pathname').should('contain', '/labeler/')
