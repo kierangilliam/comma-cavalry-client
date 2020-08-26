809c25a8ee4c603c43dbbe875a7331fe7d735c62
Kieran Gill <kieran.gill1997@gmail.com>
2020-08-26 16:07:50 -0700

    more types

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -1,3 +1,5 @@
+import type { Writable } from 'svelte/store';
+
 export * from './__generated__';
 
 export type Point = { x: number, y: number }
@@ -24,3 +26,35 @@ export type User = { name: string, email: string }
 export type DrawingMode = 'brush' | 'fill' | 'autoLine'
 
 export type ToolMode = DrawingMode | 'move'
+
+export interface EditorContext {
+    paths: Writable<Path[]>
+}
+
+// TODO Rename?
+// Used when working on a one-off mask
+export interface Entry {
+    version?: number
+    paths?: Path[]
+    archived?: boolean
+}
+
+// Used when reviewing pull requests
+// and working with multiple masks
+export interface PREntry {
+    version?: number
+    masks?: {
+        id: string
+        paths?: Path[]
+    }[]
+}
+
+export interface GitInfo {
+    username: string
+    // Authorization token
+    token: string
+    // Personal repo
+    repo: string
+    // Branch to commit to
+    branch: string
+}
