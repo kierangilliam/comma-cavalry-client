abb73c5447d460746c5b9e76eebe217ec2bf5ced
Kieran Gill <kieran.gill1997@gmail.com>
2020-08-26 17:18:29 -0700

    add keyboard shortcuts

---
+++ src/lib/components/editor/KeyboardShortcuts.svelte
@@ -0,0 +1,16 @@
+<script lang='ts'>
+    import { isDesktop } from '@lib/capacitor'
+    import { TOOL_SHORTCUTS, COLOR_SHORTCUTS } from '@lib/constants'
+    import { brushType } from './state'
+    import { setMode } from './utils'
+
+    if (isDesktop) {
+        window.addEventListener('keyup', ({ key }) => {
+            if (TOOL_SHORTCUTS[key]) {
+                setMode(TOOL_SHORTCUTS[key])
+            } else if (COLOR_SHORTCUTS[key]) {
+                $brushType = COLOR_SHORTCUTS[key]
+            }
+        })
+    }    
+</script>
