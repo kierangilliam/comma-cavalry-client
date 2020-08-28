937f12a8923d270aa3071b66c905017a6752b04d
Kieran Gill <kieran.gill1997@gmail.com>
2020-08-27 19:05:19 -0700

    add pagination

---
+++ src/pages/prs/_lib/Pagination.svelte
@@ -0,0 +1,40 @@
+<script lang='ts'>
+    import { Flex } from '@ollopa/cedar'
+    import type { PullRequest } from '@lib/types'
+    import { clamp } from '@lib/utils'
+
+    export let pr: PullRequest
+    export let index: number
+
+    const inc = () => index = clamp(index + 1, 0, pr.files.length - 1)
+    const dec = () => index = clamp(index - 1, 0, pr.files.length - 1)
+</script>
+
+<div>
+    <span>
+        PR {pr.number} ({index + 1} of {pr.files.length})
+    </span>
+    <Flex justify='between'>
+        <span class='link' on:click={dec}>prev</span>
+        <span class='link' on:click={inc}>next</span>
+    </Flex>
+</div>
+
+<style>
+    div {
+        position: absolute;
+        border-radius: var(--borderRadiusSmall);
+        background: var(--black);
+        padding: var(--s-2) var(--s-3);
+        top: calc(env(safe-area-inset-top) + 16px);     
+        left: calc(env(safe-area-inset-left) + 16px);     
+    }
+
+    .link {
+        font-weight: bolder;
+        color: var(--primary);
+    }
+    .link:hover {
+         cursor: pointer;
+    }
+</style>
