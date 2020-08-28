ae01b3a0b016e3b4aba97dd0636b0b6e3315c859
Kieran Gill <kieran.gill1997@gmail.com>
2020-08-27 22:41:00 -0700

    dispatch click

--- src/lib/components/filters/DisplacementFilter.svelte
+++ src/lib/components/filters/DisplacementFilter.svelte
@@ -1,5 +1,5 @@
 <script lang='ts'>
-    import { onMount, onDestroy } from 'svelte'
+    import { onMount, onDestroy, createEventDispatcher } from 'svelte'
     import { IMAGE_WIDTH, IMAGE_HEIGHT } from '@lib/constants'    
     import type { Point } from '@lib/types'
     import { frag, vert } from './glsl-depth-displacement'
@@ -13,7 +13,7 @@
     export let map = 'https://ik.imagekit.io/ollopa/0260_e61068239ce72500_2018-07-31--22-35-41_1_1008_Cq9e6Ou8dj.jpg' 
     export let hoverable: boolean = false
     
-    const DRIFT = drift    
+    const dispatch = createEventDispatcher()
 
     let cursor: Point
     let target: HTMLCanvasElement
@@ -109,14 +109,14 @@
             
             const scale = IMAGE_WIDTH / scaledWidthCanvas
             const size = [IMAGE_WIDTH, IMAGE_HEIGHT, scale]
-            let dx = Math.sin(time * speed) * DRIFT
-            let dy = Math.cos(time * 0.5 * speed) * DRIFT
+            let dx = Math.sin(time * speed) * drift
+            let dy = Math.cos(time * 0.5 * speed) * drift
             
             if (cursor) {
                 const percentX = cursor.x / scaledWidth
                 const percentY = cursor.y / scaledHeight
-                dx = (percentX - .5) * (DRIFT * 2) * -1
-                dy = (percentY - .5) * (DRIFT * 2) * -1
+                dx = (percentX - .5) * (drift * 2) * -1
+                dy = (percentY - .5) * (drift * 2) * -1
             }
 
             draw({ dx, dy, size })
@@ -140,7 +140,7 @@
 </script>
 
 {#if map && hasWebglSupport()}
-    <div class:hoverable {style}>
+    <div class:hoverable on:click={() => dispatch('click')} {style}>
         <canvas 
             bind:this={target}
             width={scaledWidthCanvas}
@@ -153,12 +153,19 @@
     </div>
 {:else}
     <!-- Fallback -->
-    <img class:hoverable {style} src={source} alt=''>
+    <img 
+        class:hoverable 
+        on:click={() => dispatch('click')} 
+        {style} 
+        src={source} 
+        alt=''
+    >
 {/if}
 
 <style>
     div {
         overflow: hidden;
+        transition: all 250ms;
     }
 
     .hoverable:hover {
