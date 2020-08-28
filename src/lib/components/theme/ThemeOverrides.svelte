e7d3209d86d912086d9988bf18f04016de04cf10
Kieran Gill <kieran.gill1997@gmail.com>
2020-08-27 22:42:40 -0700

    buttons -> pointer

--- src/lib/components/theme/ThemeOverrides.svelte
+++ src/lib/components/theme/ThemeOverrides.svelte
@@ -106,6 +106,9 @@
         margin: 0;
         overflow: hidden; /* ollopa mobile theme */
     }
+    :global(button:hover) {
+        cursor: pointer;
+    }
     :global(p, h1, h2, h3, h4, h5, h6) {
         margin-top: 0;
         margin-bottom: 0;
